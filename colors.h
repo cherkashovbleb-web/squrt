b5939f770fe8140a6d854ddf9b0431543de171cb
cherkashovbleb-web <cherkashovbleb@gmail.com>
2026-08-29 16:58:06 +0500

    first appload

---
+++ colors.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG
+#define CONFIG
+
+
+#define RED "\e[0;31m"
+#define YELLOW "\e[0;33m"
+#define NO_COLOR "\e[0m"
+#define BLUE "\e[0;34m"
+#define GREEN "\e[0;32m"
+#define RED_UNDERLINE "\e[4;31m"
+#define YELLOW_UNDERLINE "\e[4;33m"
+#define BLUE_UNDERLINE "\e[4;34m"
+#define GREEN_UNDERLINE "\e[4;32m"
+#define GRAY "\e[97m"
+
+
+#endif
